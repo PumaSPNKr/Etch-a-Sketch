b9a40f007cb6c2af4492bf8ce41e38796fa5cd19
Jordan <Jfma.din@gmail.com>
2025-05-05 21:50:54 -0400

    Add container for grids

---
+++ index.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 6; i++){
+    let gridContainer = document.querySelector("#container");
+    let newGrid = document.createElement("div");
+
+    // gridContainer.appendChild(newGrid);
+}
